8760da782a81e2774dd534c0a9f7366220b0e82e
cbartlo <cbartlo@gmail.com>
2015-12-07 00:08:18 +0000

    nitrous-backup at Mon Dec  7 00:08:18 UTC 2015

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,154 @@
 # Write your code here!
+def game_hash()
+  hash = {
+    home:{
+      team_name: "Brooklyn Nets",
+      colors: ["Black","White"],
+      players:[
+        "Alan Anderson" => {
+        player_name: "Alan Anderson",
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
+
+          },
+        "Reggie Evans" => {
+          player_name: "Reggie Evans",
+        number: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
+
+          },
+        "Brook Lopez" => {
+          player_name: "Brook Lopez",
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15
+
+          },
+        "Mason Plumlee" => {
+        player_name: "Mason Plumlee",
+        number: 1,
+        shoe: 19,
+        points: 26,
+        rebounds: 12,
+        assists: 6,
+        steals: 3,
+        blocks: 8,
+        slam_dunks: 5
+
+          },
+        "Jason Terry" => {
+          player_name: "Jason Terry",
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1
+
+          }
+
+        ]
+
+      },
+    away:{
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise","Purple"],
+      players:[
+        "Jeff Adrian" => {
+          player_name: "Jeff Adrian",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+          },
+        "Bismak Biyombo" => {
+          player_name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+
+          },
+        "DeSagna Diop" => {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+
+          },
+        "Ben Gordon" => {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+
+          },
+        "Brendan Haywood" => {
+          player_name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+
+          }
+
+        ]
+      }
+    }
+  return hash
+end
+
+def num_points_scored(pname)
+  the_hash = game_hash()
+  the_hash.each do |team|
+    
+  end
+
+end
 
 
-
-
-
-
+num_points_scored("Brendan Haywood")
 
 
 
